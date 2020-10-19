e6b1dafa410b1fc40e5bb6461ec3e0a2a2bf2fbe
admin01 <abanzhof@gmail.com>
2020-10-19 10:22:53 -0400

    First Commit

---
+++ Assets/game/core.clj
@@ -0,0 +1,20 @@
+(ns game.core
+  (:require [arcadia.core :refer :all]
+            [arcadia.linear :as l])
+  (:import [UnityEngine Vector3]))
+
+(defn move-randomly
+  [g _]
+  (defn xRand [] (- (rand 13) 2))
+  (defn yRand [] (+ 2 (rand 6)))
+  (defn zRand [] (+ 40 (rand 15)))
+  (def v (Vector3. (xRand) (yRand) (zRand)))
+  (set! (.. g transform position) v))
+
+(defn init
+  [_ _]
+  (hook+
+   (object-named "BlueJelly")
+   :on-mouse-over
+   :run_away
+   #'move-randomly))
