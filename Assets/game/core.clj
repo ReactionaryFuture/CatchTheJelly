ca03bb36916aa04c93e6a187bdf72b298991d33d
ReactionaryFuture <ABanzhof@gmail.com>
2020-10-20 08:21:59 -0400

    Update core.clj

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -5,11 +5,11 @@
 
 (defn move-randomly
   [g _]
-  (letfn [(defn xRand [] (- (rand 13) 2))
-          (defn yRand [] (+ 2 (rand 6)))
-          (defn zRand [] (+ 40 (rand 15))]
-  (def v (Vector3. (xRand) (yRand) (zRand)))
-  (set! (.. g transform position) v)))
+  (letfn [(xRand [] (- (rand 13) 2))
+          (yRand [] (+ 2 (rand 6)))
+          (zRand [] (+ 40 (rand 15)))]
+    (def v (Vector3. (xRand) (yRand) (zRand)))
+    (set! (.. g transform position) v)))
 
 (defn init
   [_ _]
